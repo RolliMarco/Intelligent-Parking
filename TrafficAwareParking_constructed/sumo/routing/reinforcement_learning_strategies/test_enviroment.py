cb08a84a64a538ffc98f8677164889d3bf3919b2
Marco Rolli <marcott.rolli@gmail.com>
2026-09-15 16:26:21 +0200

    erster erfolgreicher Schritt

--- TrafficAwareParking_constructed/sumo/routing/reinforcement_learning_strategies/test_enviroment.py
+++ TrafficAwareParking_constructed/sumo/routing/reinforcement_learning_strategies/test_enviroment.py
@@ -1,14 +1,53 @@
 from parking_env import ParkingEnv
 
+
+# Environment erzeugen
 env = ParkingEnv()
+
+
+# ---------------------------------------------------------
+# Episode starten
+# ---------------------------------------------------------
 
 observation, info = env.reset()
 
 print("Environment gestartet.")
-print("State:")
+
+print("Initial State:")
 print(observation)
 
 print("Info:")
 print(info)
 
+
+# ---------------------------------------------------------
+# Testaktion ausführen
+# ---------------------------------------------------------
+
+# 0 = P1
+# 1 = P2
+# 2 = P3
+
+action = 1
+
+new_observation, reward, terminated, truncated, info = env.step(action)
+
+
+# ---------------------------------------------------------
+# Ergebnisse ausgeben
+# ---------------------------------------------------------
+
+print("\nAktion:")
+print(action)
+
+print("\nReward:")
+print(reward)
+
+print("\nNeuer State:")
+print(new_observation)
+
+print("\nInfo:")
+print(info)
+
+print("\nSimulation beendet.")
 env.close()
